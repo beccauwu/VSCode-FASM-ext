cf2b3930bacfb4f34c0805cb7383b77a8767f55b
beccauwu <rebeccaperttula@gmail.com>
2026-01-06 17:27:22 +0100

    add detail to instruction/register completions

--- src/completion.ts
+++ src/completion.ts
@@ -1,25 +1,53 @@
-import * as vscode from 'vscode';
-import {fasmInstructions, fasmRegisters, fasmMacros} from "./fasm.defs"
+import * as vscode from "vscode";
+import {
+	fasmInstructions,
+	fasmRegisters,
+	fasmMacros,
+	instructions,
+	registers,
+} from "./fasm.defs";
 
 function completitionProviderString() {
-    const completionItems = [
-        ...fasmInstructions.map(instruction => 
-            new vscode.CompletionItem(instruction, vscode.CompletionItemKind.Keyword)),
-        ...fasmRegisters.map(register => 
-            new vscode.CompletionItem(register, vscode.CompletionItemKind.Variable)),
-        ...fasmMacros.map(macro => 
-            new vscode.CompletionItem(macro, vscode.CompletionItemKind.Function))
-    ];
-    return completionItems;
+	const completionItems = [
+		...fasmInstructions.map((instruction) => {
+			const res = new vscode.CompletionItem(
+				instruction,
+				vscode.CompletionItemKind.Keyword,
+			);
+			if (instruction in instructions) {
+				res.detail = instructions[instruction].description;
+			}
+			return res;
+		}),
+		...fasmRegisters.map((register) => {
+			const res = new vscode.CompletionItem(
+				register,
+				vscode.CompletionItemKind.Variable,
+			);
+      if(register in registers) {
+        res.detail = registers[register].type;
+      }
+      return res;
+		}),
+		...fasmMacros.map(
+			(macro) =>
+				new vscode.CompletionItem(macro, vscode.CompletionItemKind.Function),
+		),
+	];
+	return completionItems;
 }
 
 function completionProvider() {
-    // for buttons switch
-    vscode.commands.executeCommand('setContext', 'fasm.mode.run', true);
-    vscode.commands.executeCommand('setContext', 'fasm.mode.debug', false);
+	// for buttons switch
+	vscode.commands.executeCommand("setContext", "fasm.mode.run", true);
+	vscode.commands.executeCommand("setContext", "fasm.mode.debug", false);
 
-    // register regular expressions
-    return vscode.languages.registerCompletionItemProvider('fasm', { provideCompletionItems() { return completitionProviderString() } });    
+	// register regular expressions
+	return vscode.languages.registerCompletionItemProvider("fasm", {
+		provideCompletionItems() {
+			return completitionProviderString();
+		},
+	});
 }
 
-export default completionProvider
+export default completionProvider;
