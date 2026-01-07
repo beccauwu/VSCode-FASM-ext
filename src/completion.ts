eaf79e16126468aaa87805800214286055365a8d
beccauwu <rebeccaperttula@gmail.com>
2026-01-07 13:58:39 +0100

    add completion for local declarations

--- src/completion.ts
+++ src/completion.ts
@@ -24,10 +24,10 @@ function completitionProviderString() {
 				register,
 				vscode.CompletionItemKind.Variable,
 			);
-      if(register in registers) {
-        res.detail = registers[register].type;
-      }
-      return res;
+			if (register in registers) {
+				res.detail = registers[register].type;
+			}
+			return res;
 		}),
 		...fasmMacros.map(
 			(macro) =>
@@ -44,8 +44,48 @@ function completionProvider() {
 
 	// register regular expressions
 	return vscode.languages.registerCompletionItemProvider("fasm", {
-		provideCompletionItems() {
-			return completitionProviderString();
+		provideCompletionItems(document, _, _, _) {
+			const prefixed_decls_r =
+				/(?<prefix>(?:struc|macro|global|extrn|public))\s+(?<name>\w+)/g;
+			const suffixed_decls_r =
+				/(?<name>\w+)\s+(?<suffix>(?::|(?:file|db|dw|du|dd|dp|df|dq|dt)\??|(?:rb|rw|rd|rp|rf|rq|rt)))/g;
+			const text = document.getText();
+			const decls = new Map<string, vscode.CompletionItem>();
+			for (const match of text.matchAll(prefixed_decls_r)) {
+				if (!match.groups?.name || !match.groups?.prefix) throw "unreachable";
+				const res = new vscode.CompletionItem(match.groups.name);
+				switch (match.groups.prefix) {
+					case "struc":
+						res.kind = vscode.CompletionItemKind.Struct;
+						break;
+					case "macro":
+						res.kind = vscode.CompletionItemKind.Function;
+						break;
+					default:
+						res.kind = vscode.CompletionItemKind.Variable;
+						break;
+				}
+				res.detail = `${match.groups.prefix} ${match.groups.name}`;
+				res.sortText = "a";
+				decls.set(match.groups.name, res);
+			}
+			for (const match of text.matchAll(suffixed_decls_r)) {
+				if (!match.groups?.name || !match.groups?.suffix) throw "unreachable";
+				const res = new vscode.CompletionItem(match.groups.name);
+				switch (match.groups.suffix) {
+					case ":":
+						res.kind = vscode.CompletionItemKind.Function;
+						res.detail = `local ${match.groups.name}`;
+						break;
+					default:
+						res.kind = vscode.CompletionItemKind.Variable;
+						res.detail = `${match.groups.name}: ${match.groups.suffix}`;
+						break;
+				}
+				res.sortText = "a";
+				if (!decls.has(match.groups.name)) decls.set(match.groups.name, res);
+			}
+			return [...decls.values(), ...completitionProviderString()];
 		},
 	});
 }
